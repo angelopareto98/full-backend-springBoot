b8327a74fea69000edf65f284b9683a00ace10a8
Angelo Pareto <angelopareto98@gmail.com>
2023-03-28 18:05:03 +0300

    first commit

---
+++ src/test/java/com/anghack/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anghack.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
