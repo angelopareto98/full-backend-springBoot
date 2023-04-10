be3df64fa6eb6080a3fe4f2aa16559b4af21a0bd
Angelo Pareto <angelopareto98@gmail.com>
2023-04-10 18:35:29 +0300

    add a GlobalExceptionHandler to handle a all exception in our application

---
+++ src/main/java/com/anghack/blog/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+
+package com.anghack.blog.exception;
+
+import com.anghack.blog.payload.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ *
+ * @author anghack
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex){
+        String message = ex.getMessage();
+        ApiResponse apiResponse = new ApiResponse(message, false);
+        
+        return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+
+}
