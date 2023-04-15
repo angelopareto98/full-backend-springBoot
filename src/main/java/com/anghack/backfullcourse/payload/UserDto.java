2c714f7d361037eea48cc12103eb7705582d53b0
Angelo Pareto <angelopareto98@gmail.com>
2023-04-15 11:22:40 +0300

    user done

--- src/main/java/com/anghack/backfullcourse/payload/UserDto.java
+++ src/main/java/com/anghack/backfullcourse/payload/UserDto.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,6 +16,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class UserDto {
     private int id;
 
