2e0a6d2286a7b262a48d0eb4996c780bc1ce2b13
Angelo Pareto <angelopareto98@gmail.com>
2023-04-25 18:11:22 +0300

    add method to generate a Token

--- src/main/java/com/anghack/backfullcourse/config/JwtService.java
+++ src/main/java/com/anghack/backfullcourse/config/JwtService.java
@@ -1,12 +1,17 @@
 package com.anghack.backfullcourse.config;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
@@ -21,6 +26,18 @@ public class JwtService {
         final Claims claims = extractAllClaims(token);
 
         return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+
+        return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256).compact();
     }
 
     private Claims extractAllClaims(String token) {
