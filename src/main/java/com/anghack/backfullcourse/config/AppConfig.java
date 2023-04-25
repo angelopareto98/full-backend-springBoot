5e477e1239f5faf7179184846b1ac6deb2b2dddb
Angelo Pareto <angelopareto98@gmail.com>
2023-04-25 19:42:44 +0300

    add SecurityConfig

--- src/main/java/com/anghack/backfullcourse/config/AppConfig.java
+++ src/main/java/com/anghack/backfullcourse/config/AppConfig.java
@@ -2,7 +2,13 @@ package com.anghack.backfullcourse.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.anghack.backfullcourse.exception.ResourceNotFoundException;
 import com.anghack.backfullcourse.repository.UserRepo;
@@ -16,10 +22,29 @@ public class AppConfig {
     private final UserRepo userRepo;
 
     @Bean
-    public UserDetailsService userDetails() {
+    public UserDetailsService userDetailsService() {
         return username -> userRepo.findByEmail(username)
                 .orElseThrow(() -> new ResourceNotFoundException("User", "username " + username, 0));
 
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
